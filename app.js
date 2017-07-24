7b8eb26eba0ae79201b7e8392254285a104a37f9
Thien Thao <king@Thiens-iMac.local>
2017-07-24 19:48:51 +0700

    update ios

--- app.js
+++ app.js
@@ -27,11 +27,12 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 app.use(express.static('./public'))
 app.use(session);
+io.use(sharedsession(session));
 /*
 var ngay = new Date().toISOString().
 replace(/T/, ' '). // replace T with a space
 replace(/\..+/, '');
-io.use(sharedsession(session));
+
 io.on("connection", function(socket) {
 
     socket.on('save', function(data) {
@@ -222,7 +223,7 @@ app.get('/add', function(req, res) {
 
 
 io.on("connection",(socket) => {
-    
+    console.log(socket.id)
     socket.on('save',(url) => {
         url = url.replace(/\s/g, '');
         url = url.replace(/\(/g, '');
@@ -319,6 +320,68 @@ io.on("connection",(socket) => {
         })
     })
     
+    /* IOS */
+    Video.find({},(err,docs) => {
+        socket.emit('ios:list',docs)
+    }).sort({'_id':'desc'})
+    .limit(50).skip(0)
+
+    socket.on('ios:xemthem',function(id){
+        socket.handshake.session.xemthem = 50
+        socket.handshake.session.xemthem = Number(socket.handshake.session.xemthem + id)
+        Video.find({},function(err,docs){
+            socket.emit('ios:list', docs)
+        }).limit(Number(socket.handshake.session.xemthem)).skip(0)
+        .sort({'_id':'desc'})
+        
+       
+    })
+    socket.on('ios:seach', function(key) {
+        if (Number(key)) {
+            Video.find({
+                _id: key
+            }, function(err, docs) {
+                socket.emit('ios:list', docs)
+            }).sort({'_id' : 'desc'})
+        } else {
+            var searchQuery = new RegExp(key.toLowerCase(), 'i');
+            Video.find({
+                url: searchQuery
+            }, function(err, docs) {
+                socket.emit('ios:list', docs)
+            }).sort({'_id' : 'desc'})
+
+        }
+
+    })
+
+    socket.on('ios:view',(id) => {
+        
+        Video.findById(id,(err,doc) => {
+                    socket.emit('ios:viewVideo',doc)
+            })
+    })
+    socket.on('ios:danhdau',(id) => {
+        Video.findById(id,(err,doc) => {
+            doc.danhdau = true
+            doc.save((err,result) => {
+                if(err){
+                    socket.on('err',err)
+                }
+                if(result){
+                    Video.find({},(err,docs) => {
+                        socket.emit('ios:list',docs)
+                    })
+                }
+            })
+        })
+    })
+    socket.on('ios:list',() => {
+        Video.find({},(err,docs) => {
+        socket.emit('ios:list',docs)
+    }).sort({'_id':'desc'})
+    .limit(50).skip(0)
+    })
     
 })
 
